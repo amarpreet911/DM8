6c13b0d80525bf928d3f18bb3cd26ab7e1abc34c
Amarpreet Singh <amarpreet911@gmail.com>
2016-12-24 00:59:17 -0500

    Map_Page_Functionality classes added

--- app/src/main/java/org/dmate/com/dm8/CategorySelect.java
+++ app/src/main/java/org/dmate/com/dm8/CategorySelect.java
@@ -1,7 +1,11 @@
 package org.dmate.com.dm8;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import org.dmate.com.dm8.maps.DriverMap1;
 
 /**
  * Created by amarpreet911 on 29/11/16.
@@ -13,5 +17,9 @@ public class CategorySelect extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category_select);
         System.out.println("reached goal");
+    }
+    public void layMidClk(View view){
+        Intent intent = new Intent(CategorySelect.this, DriverMap1.class);
+        startActivity(intent);
     }
 }
