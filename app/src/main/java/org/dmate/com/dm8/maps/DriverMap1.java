a9346786f6462902dd333a50b30a98055805a30d
amarpreet911 <Donaurkaun@911>
2017-01-03 00:01:28 -0500

    commiting new code for map page

--- app/src/main/java/org/dmate/com/dm8/maps/DriverMap1.java
+++ app/src/main/java/org/dmate/com/dm8/maps/DriverMap1.java
@@ -1,13 +1,11 @@
 package org.dmate.com.dm8.maps;
 
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.provider.Settings;
+
+import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+/*import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;*/
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,12 +18,28 @@ import org.dmate.com.dm8.R;
 
 public class DriverMap1 extends FragmentActivity implements OnMapReadyCallback{
     public GoogleMap mMap;
-
+  /*  private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_page);
+       /* mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);*/
+
+        // specify an adapter (see also next example)
+      //The adapter provides access to the items in your data set, creates views for items, and
+        // replaces the content of some of the views with new data items when the original
+        // item is no longer visible.
+      /*  mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);*/
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
